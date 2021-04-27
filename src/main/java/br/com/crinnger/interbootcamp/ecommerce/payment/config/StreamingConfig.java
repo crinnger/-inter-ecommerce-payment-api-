28cac6f8ca8a444d5d9dde3cbd8d2f0bee361303
Crinnger <crinnger@gmail.com>
2021-04-26 20:20:26 -0400

    atualizando

--- src/main/java/br/com/crinnger/interbootcamp/ecommerce/payment/config/StreamingConfig.java
+++ src/main/java/br/com/crinnger/interbootcamp/ecommerce/payment/config/StreamingConfig.java
@@ -3,8 +3,10 @@ package br.com.crinnger.interbootcamp.ecommerce.payment.config;
 import br.com.crinnger.interbootcamp.ecommerce.payment.streaming.CheckoutProcessor;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @Configuration
+@EnableKafka
 @EnableBinding(value = CheckoutProcessor.class)
 public class StreamingConfig {
 }
--- src/main/java/br/com/crinnger/interbootcamp/ecommerce/payment/listener/CheckoutCreatedListener.java
+++ src/main/java/br/com/crinnger/interbootcamp/ecommerce/payment/listener/CheckoutCreatedListener.java
@@ -1,10 +1,16 @@
 package br.com.crinnger.interbootcamp.ecommerce.payment.listener;
 
 import br.com.crinnger.interbootcamp.ecommerce.payment.streaming.CheckoutProcessor;
+import com.google.gson.Gson;
 import lombok.RequiredArgsConstructor;
 import org.apache.avro.generic.GenericData;
 import org.apache.avro.generic.GenericRecord;
 import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.support.GenericMessage;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
@@ -19,17 +25,24 @@ public class CheckoutCreatedListener {
 
     private final CheckoutProcessor checkoutProcessor;
 
+
+    //public PaymentCreatedEvent handler(CheckoutCreatedEvent event){
+    //@KafkaListener(topics = "streaming.ecommerce.checkout.created-value", groupId = "inter-ecommerce")
     @StreamListener(CheckoutProcessor.INPUT)
-    public void handler(GenericMessage<GenericRecord> event){
-        //CheckoutCreatedEvent checkoutCreatedEvent= CheckoutCreatedEvent.
+    @SendTo(CheckoutProcessor.OUTPUT)
+    public PaymentCreatedEvent listenWithHeaders(
+            GenericMessage<GenericRecord> message) {
+        Gson gson = new Gson();
+        CheckoutCreatedEvent event = gson.fromJson(message.getPayload().toString(),CheckoutCreatedEvent.class);
         //processar pagamento gateway
         // salvar dados do pagamento
         // evoar evento do pagamento
         PaymentCreatedEvent paymentCreatedEvent = PaymentCreatedEvent.newBuilder()
-                .setCheckoutCode(UUID.randomUUID().toString())
+                .setCheckoutCode(event.getCheckoutCode())
                 .setCheckoutStatus("APPROVED")
                 .setPaymentCode(UUID.randomUUID().toString())
                 .build();
-        checkoutProcessor.output().send(MessageBuilder.withPayload(paymentCreatedEvent).build());
+        //checkoutProcessor.output().send(MessageBuilder.withPayload(paymentCreatedEvent).build());
+        return  paymentCreatedEvent;
     }
 }
